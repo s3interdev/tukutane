7dd53d90b5926834568877720b3e4b0772bc6fe4
Michael Owuor <michael.owuor@gmail.com>
2021-06-27 20:26:25 +0300

    build: create date and time pickups.

--- src/views/meetups/MeetupsOrganize.vue
+++ src/views/meetups/MeetupsOrganize.vue
@@ -63,6 +63,17 @@
 					</v-row>
 					<v-row>
 						<v-col>
+							<h4>MEETUP DATE AND TIME</h4>
+						</v-col>
+					</v-row>
+					<v-row>
+						<v-date-picker v-model="date"></v-date-picker>
+					</v-row>
+					<v-row>
+						<v-time-picker v-model="time" format="24hr"></v-time-picker>
+					</v-row>
+					<v-row>
+						<v-col>
 							<v-btn class="accent" :disabled="!validForm" type="submit">
 								Organize Meetup
 							</v-btn>
@@ -84,6 +95,10 @@ export default {
 			location: '',
 			imageURL: '',
 			description: '',
+			date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+				.toISOString()
+				.substr(0, 10),
+			time: new Date(),
 		};
 	},
 
@@ -98,7 +113,7 @@ export default {
 				location: this.location,
 				imageURL: this.imageURL,
 				description: this.description,
-				date: new Date(),
+				date: this.selectedDateTime,
 			};
 
 			this.$store.dispatch('organizeMeetup', meetupData);
@@ -114,6 +129,23 @@ export default {
 				this.imageURL !== '' &&
 				this.description !== ''
 			);
+		},
+
+		selectedDateTime() {
+			const date = new Date(this.date);
+
+			if (typeof this.time === 'string') {
+				let hours = this.time.match(/^(\d+)/)[1];
+				let minutes = this.time.match(/:(\d+)/)[1];
+
+				date.setHours(hours);
+				date.setMinutes(minutes);
+			} else {
+				date.setHours(this.time.getHours());
+				date.setMinutes(this.time.getMinutes());
+			}
+
+			return date;
 		},
 	},
 };
